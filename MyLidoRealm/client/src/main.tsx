9983bf5621b22906481520c65941bbfd1484e96b
Lidoservermc <lidosservermc@proton.me>
2025-05-01 21:00:51 -0400

    Delete MyLidoRealm directory

--- MyLidoRealm/client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Add title to the page
-document.title = "MyLido - Lido's Minecraft Realm";
-
-createRoot(document.getElementById("root")!).render(<App />);
